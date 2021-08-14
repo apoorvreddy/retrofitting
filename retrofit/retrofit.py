24e2081117fb7819493d4a749820bc8057532446
Apoorv R. Arrabothu <apoorv.arrabothu@walmart.com>
2021-08-14 14:58:14 +0530

    Add minor optimizations

--- retrofit/retrofit.py
+++ retrofit/retrofit.py
@@ -6,7 +6,7 @@ from scipy import sparse
 from tqdm import tqdm
 	
 class RetroFit(object):
-	def __init__(self, iterations=10, alpha=1.0, beta=None):
+	def __init__(self, iterations=2, alpha=1.0, beta=None):
 		self.iterations = iterations
 		self.alpha = alpha
 		self.beta = beta
@@ -15,23 +15,32 @@ class RetroFit(object):
 		newVectors = Vectors(vectors)
 		# number of vertices in the graph
 		numV = max(graph.nodes) + 1
-		# initialize Beta matrix
-		if self.beta is None:
-			self.beta = np.zeros((numV, numV))
-			for u in graph.vertices():
-				indegree = graph.getInDegree(u)
-				nbrs = graph.neighbors(u)
-				self.beta[u][nbrs] = 1.0/indegree
+		# denominatorCache stores the denominator of the update equation for each node in the graph
 		# cache the denominator - 1 time computation
 		denominatorCache = {}
-		for u in range(numV):
-			denominatorCache[u] = self.beta[u].sum() + self.alpha*graph.getInDegree(u)
-		# sparsify beta
-		betasp = sparse.csr_matrix(self.beta)
-		for it in tqdm(range(self.iterations)):
-			for u in tqdm(range(numV), leave=False):
-				if graph.getInDegree(u) > 0:
-					numerator = sparse.csr_matrix.dot(betasp.getrow(u), newVectors.vectors) + self.alpha * vectors.vectors[u]
-					denominator = denominatorCache[u]
-					newVectors.vectors[u] = numerator/denominator
+		if self.beta is None:
+			# if beta is not specified, set beta_ij to 1/indegree(i)
+			# update equation is faster too as this removes need for matrix multiplies
+			# sum of eta_ij over j is 1
+			for u in range(numV):
+				denominatorCache[u] = 1.0 + self.alpha*graph.getInDegree(u)
+			for it in tqdm(range(self.iterations)):
+				for u in tqdm(range(numV), leave=False):
+					if graph.getInDegree(u) > 0:
+						nbrs = graph.neighbors(u)
+						numerator = newVectors.vectors[nbrs].sum(axis=0)/(1.0 * graph.getInDegree(u)) + self.alpha * vectors.vectors[u]
+						denominator = denominatorCache[u]
+						newVectors.vectors[u] = numerator/denominator
+		else:
+			# handle the general case where beta is specified
+			for u in range(numV):
+				denominatorCache[u] = self.beta[u].sum() + self.alpha*graph.getInDegree(u)
+			# sparsify beta
+			betasp = sparse.csr_matrix(self.beta)
+			for it in tqdm(range(self.iterations)):
+				for u in tqdm(range(numV), leave=False):
+					if graph.getInDegree(u) > 0:
+						numerator = sparse.csr_matrix.dot(betasp.getrow(u), newVectors.vectors) + self.alpha * vectors.vectors[u]
+						denominator = denominatorCache[u]
+						newVectors.vectors[u] = numerator/denominator
 		return newVectors
--- wordnet_driver.py
+++ wordnet_driver.py
@@ -13,11 +13,9 @@ nlp = spacy.load('en_core_web_md')
 # take unigram and word-only tokens from wordnet
 wordnetVocab = [word for word in wn.words() if '_' not in word and word.isalpha() and '-' not in word]
 
-print("len1", len(wordnetVocab))
-#
+# get WordVectors
 doc = ' '.join(wordnetVocab)
 XV = nlp(doc)
-print("lenXV", len(XV))
 
 # take intersection of words in wordnetVocab and spacy's wordvector vocab
 finalVocab = []
@@ -51,8 +49,7 @@ for word in finalVocab:
 
 graph = Graph(edges)
 
-
 # ## RetroFit
-# from retrofit.retrofit import RetroFit
-# retro = RetroFit(V, graph)
-# newVectors = retro.fit(verbose=True)
+from retrofit.retrofit import RetroFit
+retro = RetroFit()
+newVectors = retro.fit(V, graph)
